98eb4a4d26c23c87e72b3fe1b83b141d8661d4c2
Beakjiyeon <bjy1530@naver.com>
2019-07-07 12:45:02 +0900

    face detect

---
+++ app/src/main/java/com/example/face3/MainActivity.java
@@ -0,0 +1,216 @@
+package com.example.face3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.io.*;
+import android.app.*;
+import android.content.*;
+import android.net.*;
+import android.os.*;
+import android.util.Log;
+import android.view.*;
+import android.graphics.*;
+import android.widget.*;
+import android.provider.*;
+// 출력원함
+// This sample uses Apache HttpComponents:
+// http://hc.apache.org/httpcomponents-core-ga/httpcore/apidocs/
+// https://hc.apache.org/httpcomponents-client-ga/httpclient/apidocs/
+// https://docs.microsoft.com/ko-kr/azure/cognitive-services/face/quickstarts/java
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+// 추가
+import com.microsoft.projectoxford.face.*;
+import com.microsoft.projectoxford.face.contract.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 사용자가 그림을 선택할 수 있도록 새 작업을 시작하는 이벤트 처리기가 단추에 생성됩니다. ImageView에 사진이 표시됩니다.
+    private final int PICK_IMAGE = 1;
+    private ProgressDialog detectionProgressDialog;
+
+    // onCreate 메소드 위에 추가
+    // Replace `<API endpoint>` with the Azure region associated with
+// your subscription key. For example,
+// apiEndpoint = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0"
+    private final String apiEndpoint = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0";
+
+    // Replace `<Subscription Key>` with your subscription key.
+// For example, subscriptionKey = "0123456789abcdef0123456789ABCDEF"
+    private final String subscriptionKey = "db695d342d624486ab1fa259ca81a418";
+
+    private final FaceServiceClient faceServiceClient =
+            new FaceServiceRestClient(apiEndpoint, subscriptionKey);
+
+    // private final FaceServiceClient faceServiceClient = new FaceServiceRestClient(apiEndpoint, subscriptionKey);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(
+                        intent, "Select Picture"), PICK_IMAGE);
+            }
+        });
+
+        detectionProgressDialog = new ProgressDialog(this);
+
+
+
+    }// onCreate
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK &&
+                data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                Log.d("백지연","6");
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(
+                        getContentResolver(), uri);
+                ImageView imageView = findViewById(R.id.imageView1);
+                imageView.setImageBitmap(bitmap);
+
+                // Comment out for tutorial
+                // 주석달기
+                detectAndFrame(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    // 메소드 추가
+    // Detect faces by uploading a face image.
+// Frame faces after detection.
+    private void detectAndFrame(final Bitmap imageBitmap) {
+        Log.d("백지연","ss");
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        ByteArrayInputStream inputStream =
+                new ByteArrayInputStream(outputStream.toByteArray());
+
+        AsyncTask<InputStream, String, Face[]> detectTask =
+                new AsyncTask<InputStream, String, Face[]>() {
+                    String exceptionMessage = "";
+
+                    @Override
+                    protected Face[] doInBackground(InputStream... params) {
+                        try {
+                            publishProgress("Detecting...");
+                            Face[] result = faceServiceClient.detect(
+                                    params[0],
+                                    true,         // returnFaceId
+                                    false,        // returnFaceLandmarks
+                                    null          // returnFaceAttributes:
+                                /* new FaceServiceClient.FaceAttributeType[] {
+                                    FaceServiceClient.FaceAttributeType.Age,
+                                    FaceServiceClient.FaceAttributeType.Gender }
+                                */
+                            );
+                            faceServiceClient.createPersonGroup("그룹1","x2","y2");
+                            Log.d("백지연","그룹1");
+                            if (result == null){
+                                publishProgress(
+                                        "Detection Finished. Nothing detected");
+                                return null;
+                            }
+                            publishProgress(String.format(
+                                    "Detection Finished. %d face(s) detected",
+                                    result.length));
+                            return result;
+                        } catch (Exception e) {
+                            exceptionMessage = String.format(
+
+                                    "Detection failed: %s", e.getMessage());
+                            Log.e("create person group","에러가 도대체 왜 나는 것인가0");
+                            return null;
+                        }
+
+
+
+
+                    }
+
+
+                    @Override
+                    protected void onPreExecute() {
+                        //TODO: show progress dialog
+                        detectionProgressDialog.show();
+                    }
+                    @Override
+                    protected void onProgressUpdate(String... progress) {
+                        //TODO: update progress
+                        detectionProgressDialog.setMessage(progress[0]);
+                    }
+                    @Override
+                    protected void onPostExecute(Face[] result) {
+                        //TODO: update face frames
+                        detectionProgressDialog.dismiss();
+
+                        if(!exceptionMessage.equals("")){
+                            showError(exceptionMessage);
+                        }
+                        if (result == null) return;
+
+                        ImageView imageView = findViewById(R.id.imageView1);
+                        imageView.setImageBitmap(
+                                drawFaceRectanglesOnBitmap(imageBitmap, result));
+                        imageBitmap.recycle();
+                    }
+                };
+
+        detectTask.execute(inputStream);
+    }
+
+    private void showError(String message) {
+        Log.d("백지연","aa");
+        new AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }})
+                .create().show();
+    }
+    // 추가
+    private static Bitmap drawFaceRectanglesOnBitmap(
+
+            Bitmap originalBitmap, Face[] faces) {
+        Bitmap bitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        Log.d("백지연","xx");
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(10);
+        if (faces != null) {
+            for (Face face : faces) {
+                FaceRectangle faceRectangle = face.faceRectangle;
+                canvas.drawRect(
+                        faceRectangle.left,
+                        faceRectangle.top,
+                        faceRectangle.left + faceRectangle.width,
+                        faceRectangle.top + faceRectangle.height,
+                        paint);
+            }
+        }
+        return bitmap;
+    }
+}// class mainactivity
